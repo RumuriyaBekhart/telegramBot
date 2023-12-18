0d5e13d6f9fd67e37d0abccb4f50e11cd39f4e93
Rumuriya Bekhart <spb1268148@gmail.com>
2023-12-19 00:15:30 +0300

    Создала список ответов

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 from constants import TOKEN, COMANDS
 from character import PERHOONA
+from answers import ANSWERS
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -21,16 +22,21 @@ def help_mi(message):
 def photo(message):
     with open(PERHOONA['photo'], 'rb') as photo:
         bot.send_photo(chat_id=message.chat.id, photo=photo)
+    bot.send_message(message.from_user.id, '''Ну разве не милашка?))''')
 
 
 def music(message):
-    # with open(PERHOONA['music'], 'rb') as photo:
-    #     bot.send_photo(chat_id=message.chat.id, photo=photo)
-    pass
+    audio = open(PERHOONA['music'], 'rb')
+    bot.send_chat_action(message.from_user.id, 'upload_audio')
+    bot.send_audio(message.from_user.id, audio)
+    audio.close()
+    bot.send_message(message.from_user.id, '''Вот эта моя любимая)''')
 
 
 def info(message):
-    pass
+    bot.send_message(message.from_user.id, '''
+    Нуу... Зовут - Перона. Ляблю милых зверушек и сладости. Фрукт - Хоро-Хоро но ми(если это тебе что-либо говорит значит мы поладим)
+    ''')
 
 
 # Обрабатывает все отправленные документы и аудиофайлы
@@ -45,6 +51,7 @@ def docs_audio(message):
 def get_text_messages(message):
     if message.text == "Привет":
         bot.send_message(message.from_user.id, "Приветик)")
+        bot.send_message(message.from_user.id, "Как дела?")
     elif 'help' in message.text:
         help_mi(message)
     elif 'start' in message.text:
@@ -55,14 +62,18 @@ def get_text_messages(message):
         music(message)
     elif 'info' in message.text:
         info(message)
-    elif 'тебе лет?' in message.text:
+    elif 'тебе лет?' in message.text.lower():
         bot.send_message(message.from_user.id, '''У девушек такое не спрашивают вобще то (> n <)''')
-    elif 'ответь' in message.text or 'Ау' in message.text:
+    elif 'ответь' in message.text.lower() or 'ау' in message.text.lower():
         bot.send_message(message.from_user.id, '''Если не отвечаю, значит занята...''')
-    elif '' in message.text:
-        pass
-    elif '' in message.text:
-        pass
+    else:
+        for world in ANSWERS.keys():
+            if world in message.text.lower():
+                bot.send_message(message.from_user.id, ANSWERS[world])
+                break
+        else:
+            bot.send_message(message.from_user.id, '''Прости, что?''')
+            bot.send_message(message.from_user.id, '''O ^ O''')
 
 
 bot.infinity_polling()
